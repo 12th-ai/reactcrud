70b571ba15bdd022cf7c7a3bf10accb3863f8d4f
mihigo prince <mihigofololive@gmail.com>
2024-01-27 20:29:55 +0200

    third commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,27 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-import Insert from './Insert'
-
-import Userlist from './userlist';
+import React from 'react'
 
 function App() {
   return (
     <div>
-      <h1>welcome to my challenge</h1>
-
-
-
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/userlist" element={<Userlist />} />
-          </Routes>
-        </div>
-        <Insert />
-      </Router>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
